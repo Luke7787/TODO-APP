b99396c315b5c9ecfbb2741d290f364e27bbd00c
Luke Zhuang <Lukezhuang@yahoo.com>
2025-11-07 17:33:56 -0800

    set up Expo Go on mobile for testing and updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>
+        Edit app/index.tsx to edit this screen.1233
+      </Text>
+      <Text>hi</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    gap: 10,
+  },
+  content: {
+    fontSize: 22,
+  },
+});
