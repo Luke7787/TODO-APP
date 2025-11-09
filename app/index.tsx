f2b6aecaabcee2aafb10eede3dbfa1e2843f2b30
Luke Zhuang <Lukezhuang@yahoo.com>
2025-11-08 18:30:30 -0800

    added a button nav to another page in index.tsx, created new page about.tsx and updated page's names in layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
+import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
-        Edit app/index.tsx to edit this screen.1233
+        Edit app/index.tsx to edit this screen.
       </Text>
       <Text>hi</Text>
+
+      <Link href={"/about"}>Visit about screen</Link>
     </View>
   );
 }
@@ -14,10 +17,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
     gap: 10,
   },
   content: {
